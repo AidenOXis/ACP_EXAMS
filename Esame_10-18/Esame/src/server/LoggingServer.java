2c350017843756e1d24d22ea50b7bed7517377a2
Gennaro <155782391+AidenOXis@users.noreply.github.com>
2025-07-24 18:37:52 +0200

    Update LoggingServer.java

--- Esame_10-18/Esame/src/server/LoggingServer.java
+++ Esame_10-18/Esame/src/server/LoggingServer.java
@@ -1,5 +1,16 @@
 package server;
 
 public class LoggingServer {
+    public static void main(String[] args){
+        System.out.println("[LoggingServer]Server started...");
+        if(args.length != 1){
+            System.out.println("Uso: java LoggingServer <porta>");
+            return; 
+        }
+        int port = Integer.parseInt(args[0]); 
+        LoggingImpl impl = new LoggingImpl(port); 
+        impl.runSkeleton();
+    }
     
 }
+
